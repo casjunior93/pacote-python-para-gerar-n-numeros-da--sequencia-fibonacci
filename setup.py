6f2c42602838e1d7acea20e90450d8d6a3ef3e8c
Carlos Alberto Silva Júnior <56141054+casjunior93@users.noreply.github.com>
2022-03-24 16:22:22 -0300

    Link do Github

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="sequencia_fibonacci",
+    name="sequence_fibonacci",
     version="0.0.1",
     author="Carlos Alberto Silva Júnior",
     author_email="casjunior93@gmail.com",
     description="Returns a sequence of n Fibonacci numbers.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/casjunior93/pacote-python-para-gerar-n-numeros-da--sequencia-fibonacci"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
